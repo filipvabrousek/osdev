b935e66e81e70526b636372df81525097c37947f
Filip Vabrousek <filipvabrousek1@gmail.com>
2024-07-01 11:56:13 +0200

    Add files via upload

--- FrOS-minimal-c-kernel/kernel.c
+++ FrOS-minimal-c-kernel/kernel.c
@@ -1,8 +1,9 @@
-/* This will force us to create a kernel entry function instead of jumping to kernel.c:0x00 */
-void dummy_test_entrypoint() {
-}
-
 void main() {
     char* video_memory = (char*) 0xb8000;
     *video_memory = 'L';
 }
+
+/*
+segmentation - required for protected mode
+paging - optional, but highly recommended
+ */
